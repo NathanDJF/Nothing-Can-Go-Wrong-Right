0976a55b597bdecaf14d3c02a6a8f09f0a4f6614
NathanDJF <nathandjfcodes@gmail.com>
2025-02-19 17:15:04 +0700

    Changed Resolution

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ import os
 from button import Button
 
 # screen
-screen = pygame.display.set_mode((900, 500))
+screen = pygame.display.set_mode((1044, 696))
 pygame.display.set_caption('Nothing Can Go Wrong, Right?')
 
 # images
 money_button_img = pygame.image.load(os.path.join('Assets/images/money button placeholder.png'))
 programming_job_button_img = pygame.image.load(os.path.join('Assets/images/programming job button placeholder.png'))
+robbery_job_button_img = pygame.image.load(os.path.join('Assets/images/robbery job button placeholder.png'))
 
 money_button = Button(200, 200, money_button_img)
 programming_job_button = Button(140, 180, programming_job_button_img)
+robbery_job_button = Button(180, 140, robbery_job_button_img)
 
+# money button + jobs
 money_button_menu = False
 def money_button_func():
     global money_button_menu
@@ -23,13 +26,16 @@ def money_button_func():
     
     if money_button_menu:
         if programming_job_button.draw():
-            print("hi")
+            screen.fill((0, 0, 0))
+            money_button_menu = False
+        if robbery_job_button.draw():
+            print("test")
             money_button_menu = False
             
 clock = pygame.time.Clock()
 while True:
     clock.tick(60)
-    screen.fill('white')
+    screen.fill((255, 255, 255))
     money_button_func()
     
     for event in pygame.event.get():
